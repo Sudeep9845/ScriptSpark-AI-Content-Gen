f7292b68fd7988a44a775b607fee329e6218334a
Sudeep9845 <sudeepsjoshi9899@gmail.com>
2024-06-29 02:32:52 +0530

    Hide_Updated Commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 /** @type { import("drizzle-kit").Config } */
 export default {
   schema: "./utils/schema.tsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:nZDU9EBwCH8I@ep-dry-frost-a5sx8kn3.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require",
+    url: process.env.POSTGRES_URL,
   },
 };
